fbe1193b91c32668baf167c612fb22cde0ee4420
Salacc_I4 <66245600+SalaccI4@users.noreply.github.com>
2025-09-10 11:58:51 -0700

    Add lowercase function to human choice
    Prevent capitalization from interfeting with human input

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 let randNum = Math.floor(Math.random() * 3) 
 let humanInput
+let humanScore = 0
+let computerScore = 0
 
 function getComputerChoice(){ 
     if (randNum == 0){
@@ -13,5 +15,12 @@ function getComputerChoice(){
 
 function getHumanChoice(){
     humanInput = prompt("Choose: rock, paper, or scissor")
-    return humanInput
+    return humanInput.toLowerCase()
 }
+
+// function playRound(humanChoice, playerChoice){
+//     if (humanChoice == playerChoice){
+//         return "Draw! Both sides picked " + humanChoice
+//     }
+//     else if (humanChoice == playerChoice)
+// }
