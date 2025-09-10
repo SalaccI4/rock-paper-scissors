ca493f1e87b34260f15b1fc0c5d635dc50e54e88
Salacc_I4 <66245600+SalaccI4@users.noreply.github.com>
2025-09-10 12:12:12 -0700

    Add logic to complete a single round
    Allow logic to dictate who wins a round in the game

--- javascript.js
+++ javascript.js
@@ -2,6 +2,8 @@ let randNum = Math.floor(Math.random() * 3)
 let humanInput
 let humanScore = 0
 let computerScore = 0
+const humanSelection = getHumanChoice()
+const computerSelection = getComputerChoice()
 
 function getComputerChoice(){ 
     if (randNum == 0){
@@ -18,9 +20,33 @@ function getHumanChoice(){
     return humanInput.toLowerCase()
 }
 
-// function playRound(humanChoice, playerChoice){
-//     if (humanChoice == playerChoice){
-//         return "Draw! Both sides picked " + humanChoice
-//     }
-//     else if (humanChoice == playerChoice)
-// }
+function playRound(humanChoice, computerChoice){
+    if (humanChoice == computerChoice){
+        return "Draw! Both sides picked " + humanChoice
+    }
+    else if (humanChoice == "rock" && computerChoice == "scissor"){
+        humanScore++
+        return "You Win! Rock beats Scissor!"
+    }
+    else if (humanChoice == "rock" && computerChoice == "paper"){
+        computerScore++
+        return "You Lose! Paper beats Rock!"
+    }
+    else if (humanChoice == "paper" && computerChoice == "rock"){
+        humanScore++
+        return "You Win! Paper beats Rock!"
+    }
+    else if (humanChoice == "paper" && computerChoice == "scissor"){
+        computerScore++
+        return "You Lose! Scissor beats Paper!"
+    }
+    else if (humanChoice == "scissor" && computerChoice == "rock"){
+        computerScore++
+        return "You Lose! Rock beats Scissor!"
+    }
+    else if (humanChoice == "scissor" && computerChoice == "paper"){
+        humanScore++
+        return "You Win! Scissor beats Paper!"
+    }
+    return "error"
+}
